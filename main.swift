a269d7f58a1da2074e5e061068e20320bd4244bd
Kholoud Almutairi <kholoudalmutairi@MacBook-Air-alkhas-b-Kholoud.local>
2022-08-04 00:55:34 +0300

    add Myfile

---
+++ main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  readlinefun
+//
+//  Created by Kholoud Almutairi on 06/01/1444 AH.
+//
+
+import Foundation
+
+print("What is the quote?")
+
+var quote = readLine()
+
+print("Who said it?")
+
+var name = readLine()
+
+print("\(name),\(quote)")

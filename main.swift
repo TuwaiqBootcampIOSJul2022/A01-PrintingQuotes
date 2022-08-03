db868e68b2171378837bfb9be4e6ace3037bae6c
user <user@MacBook-Pro-alkhas-b-user.local>
2022-08-03 23:10:51 +0300

    my code

---
+++ main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  MarcusCicero
+//
+//  Created by user on 05/01/1444 AH.
+//
+
+import Foundation
+
+print("What is the quote?")
+var name = readLine()
+
+print("Who said it?")
+
+var name1 = readLine()
+
+print(name1!,terminator: " ")
+print(name!)

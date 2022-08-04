590c1ba2a0615b19b55db6e33f297e6ea59eccec
Anaal Albeeshi <anaalalbeeshi@alMacBook-Pro-alkhas-bAnaal.local>
2022-08-04 09:50:07 +0300

    finish

---
+++ albeeshi/albeeshi/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  albeeshi
+//
+//  Created by Anaal Albeeshi on 06/01/1444 AH.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("What is the quote?")
+     let quote = readLine()
+print(" What is a specialty")
+     let specialty = readLine()
+print("\(specialty)"+" says, "+"\"\(specialty)\"")
+

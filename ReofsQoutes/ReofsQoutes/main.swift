54c696ab8cd14d1163a1d6c76d55f7d39491de6f
Reof <ree.1d@outlook.sa>
2022-08-04 03:22:55 +0300

    update : Reof Project Added

---
+++ ReofsQoutes/ReofsQoutes/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  ReofsQoutes
+//
+//  Created by REOF ALMESHARI on 04/08/2022.
+//
+
+import Foundation
+
+var quote = ""
+var author = ""
+print("What is the quote?")
+
+if let userQuote = readLine(){
+     quote = userQuote
+}
+
+print("Who said it?")
+if let userAuthor = readLine(){
+    author = userAuthor
+}
+
+print("--------------------")
+print("\(author) says , \"\(quote)\" ")
+
+//Be the change that you wish to see in the world.
+//Mahatma Gandhi
+
+

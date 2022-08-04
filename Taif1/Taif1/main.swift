7a7f67e8daf00eae693817d2fda2d7f3ef374eee
طيُف <taif..@MBPalkhasbtyuf.lan>
2022-08-04 08:43:45 +0300

    Taif Alhafi

---
+++ Taif1/Taif1/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Taif1
+//
+//  Created by طيُف .. on 04/08/2022.
+//
+
+import Foundation
+print("What is quote?")
+var quote = readLine()
+print("who said it?")
+var auther = readLine()
+print("\(auther!) says, \(quote!)")
+

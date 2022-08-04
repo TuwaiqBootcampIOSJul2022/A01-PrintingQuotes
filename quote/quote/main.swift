9d747eb5c3edc30c8b1e8bcf3a5ff7d903267326
Razan Abdullah <razanabdullah@Razans-MacBook-Pro.local>
2022-08-04 09:30:20 +0300

    homework finish

---
+++ quote/quote/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  quote
+//
+//  Created by Razan Abdullah on 06/01/1444 AH.
+//
+
+import Foundation
+
+print("Enter the quote")
+
+let quote = readLine()
+
+print("Enter the author")
+
+let author = readLine()
+
+print((author!) ,"says,",(quote!))
+
+

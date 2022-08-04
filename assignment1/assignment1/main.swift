627ce7f5f71a19b4f47bd00051041c211a69f381
Rashed Shrahili <rashedshrahili@Rasheds-MacBook-Air.local>
2022-08-04 06:46:31 +0300

    Rashed Sharahili

---
+++ assignment1/assignment1/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  assignment1
+//
+//  Created by Rashed Shrahili on 06/01/1444 AH.
+//
+
+import Foundation
+
+print("What is the quote? ")
+var qoute = readLine()
+
+print("Who said it? ")
+var author = readLine()
+
+print("\(author!) says, \"\(qoute!)\"")
+
